10b0798912e15bed34f3a3892dc4e4fb9b5f8e89
shafeequegithub <shafeequemohd711@gmail.com>
2025-02-19 13:28:22 +0530

    completed

--- lib/view/home_screen/home_scrren.dart
+++ lib/view/home_screen/home_scrren.dart
@@ -11,77 +11,120 @@ class HomeScrren extends StatefulWidget {
 }
 
 class _HomeScrrenState extends State<HomeScrren> {
-  int currentQuestionIndex = 0;
+  Color getcolor(int index) {
+    if (selectedIndex != null) {
+      if (Dummydb.questions[currentIndex]["answer index"] == selectedIndex &&
+          index == selectedIndex) {
+        return Colors.green;
+      } else if (Dummydb.questions[currentIndex]["answer index"] !=
+              selectedIndex &&
+          index == selectedIndex) {
+        return Colors.red;
+      } else if (Dummydb.questions[currentIndex]["answer index"] == index) {
+        return Colors.green;
+      }
+    }
+
+    return Colors.grey;
+  }
+
+  int currentIndex = 0;
+  int? selectedIndex;
+  int? count;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.grey.shade800),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          actions: [
+            Container(
+              color: Colors.white,
               child: Text(
-                Dummydb.questions[currentQuestionIndex]["question"].toString(),
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
+                '${currentIndex + 1}/${Dummydb.questions.length}',
+                style: TextStyle(color: Colors.black),
               ),
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Column(
-            children: List.generate(4, (index) {
-              List answerOption =
-                  Dummydb.questions[currentQuestionIndex]["options"] as List;
-
-              return optionscard(
-                  borderColor: Colors.grey,
-                  onTap: () {
-                    if (index ==
-                        Dummydb.questions[currentQuestionIndex]
-                            ["answer index"]) {
-                      print("right answer");
-                    } else {
-                      print("wrong answer");
-                    }
-                  },
-                  option: answerOption[index]);
-            }),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          InkWell(
-            onTap: () {
-              if (currentQuestionIndex < Dummydb.questions.length - 1) {
-                currentQuestionIndex++;
-                print(currentQuestionIndex);
-                setState(() {});
-              }
-            },
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15), color: Colors.blue),
-              child: Center(
-                child: Text(
-                  "Next",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
+            )
+          ],
+        ),
+        backgroundColor: Colors.black,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 60),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.shade800),
+                    child: Text(
+                      Dummydb.questions[currentIndex]["question"].toString(),
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ),
-              ),
+                SizedBox(
+                  height: 30,
+                ),
+                Column(
+                    children: List.generate(4, (index) {
+                  List optionss =
+                      Dummydb.questions[currentIndex]["options"] as List;
+                  return optionscard(
+                      bordercolor: getcolor(index),
+                      onTap: () {
+                        if (selectedIndex == null) {
+                          selectedIndex = index;
+                          setState(() {});
+                          if (index ==
+                              Dummydb.questions[currentIndex]["answer index"]) {
+                            print("right answer");
+                          } else {
+                            print("WrongAnswer");
+                          }
+                        }
+                      },
+                      options: optionss[index]);
+                })),
+                SizedBox(
+                  height: 15,
+                ),
+                InkWell(
+                  onTap: () {
+                    selectedIndex = null;
+                    if (currentIndex < Dummydb.questions.length - 1) {
+                      print(currentIndex);
+                      currentIndex++;
+                    }
+
+                    setState(() {});
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.blue),
+                    child: Center(
+                      child: Text(
+                        "next",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
@@ -89,30 +132,36 @@ class _HomeScrrenState extends State<HomeScrren> {
 
 class optionscard extends StatelessWidget {
   const optionscard(
-      {super.key, required this.option, this.onTap, this.borderColor});
-  final String option;
+      {super.key,
+      required this.options,
+      this.answerIndex,
+      this.onTap,
+      required this.bordercolor});
+  final String options;
+  final answerIndex;
   final void Function()? onTap;
-  final Color? borderColor;
+  final Color? bordercolor;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        InkWell(
-          onTap: onTap,
-          child: Container(
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
             margin: EdgeInsets.symmetric(vertical: 10),
             padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             width: double.infinity,
             decoration: BoxDecoration(
-              border: Border.all(width: sqrt1_2, color: Colors.grey),
+              border:
+                  Border.all(width: sqrt1_2, color: bordercolor ?? Colors.grey),
               borderRadius: BorderRadius.circular(10),
             ),
             child: Row(
               children: [
                 Expanded(
                   child: Text(
-                    option,
+                    options,
                     style: TextStyle(
                       fontSize: 20,
                       color: Colors.white,
@@ -127,8 +176,8 @@ class optionscard extends StatelessWidget {
               ],
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
